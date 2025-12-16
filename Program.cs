752cc011df693af62f0c2a5e4a73611dd4506ebe
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-12-16 08:49:38 +0100

    add more lines

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿string[] names = ["Zoltan", "Fanni"];
 
-Console.WriteLine($"Hello {string.Join(", ", names)}!");
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"Hello {string.Join(", ", names[..^1])} and {names[^1]}!");
 
 Console.WriteLine("tis is a new line");
+
+
+Console.ResetColor();
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
